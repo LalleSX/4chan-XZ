ec2284bfb97d47617b30a88baf82c95793a5b153
Lalle <29478339+LalleSX@users.noreply.github.com>
2023-05-10 19:17:57 +0200

    A new start

--- src/platform/$$.ts
+++
@@ -1,4 +0,0 @@
-import { d } from "../globals/globals"
-
-const $$ = (selector, root = d.body) => [...Array.from(root.querySelectorAll(selector))]
-export default $$
--- src/types/jsx.d.ts
+++
@@ -1,6 +0,0 @@
-import { EscapedHtml } from "../globals/jsx"
-
-declare namespace JSX {
-  interface IntrinsicElements extends Record<string, unknown> { }
-  interface Element extends EscapedHtml { }
-}
